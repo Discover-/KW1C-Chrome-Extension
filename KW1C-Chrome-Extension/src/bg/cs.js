be2d91923566fa090c73a63f238dfa353cfa51b3
Discover- <amort11@hotmail.com>
2014-09-07 20:41:13 +0200

    First attempt to automatically click the 'Toon rooster' button to show the class schedule when page is loaded (todo: need delay event of some kind)

--- KW1C-Chrome-Extension/src/bg/cs.js
+++ KW1C-Chrome-Extension/src/bg/cs.js
@@ -14,9 +14,20 @@ chrome.runtime.sendMessage({method: "getLocalStorage", key: "store.settings.pass
 {
 	if (response.data == "undefined")
 		return;
-		
+	
 	var body = document.body;
 	var textContent = body.textContent || body.innerText;
+	
+	if (-1 != document.URL.indexOf("stud_rooster.aspx"))
+	{
+		try
+		{
+			//@todo: delay this until it loaded the html of the page
+			document.getElementById('ctl00_ContentPlaceHolder1_btnVerder').click();
+			return;
+		}
+		catch(e){}
+	}
 
 	//! If the user's account information is incorrect...
 	if (-1 != textContent.indexOf("Je kunt niet worden aangemeld bij Koning Willem I College. Controleer je gebruikersnaam en wachtwoord en probeer het opnieuw."))
@@ -26,7 +37,8 @@ chrome.runtime.sendMessage({method: "getLocalStorage", key: "store.settings.pass
 	}
 	
 	var password = (response.data !== undefined) ? response.data : "";
-	try{
+	try
+	{
 		document.getElementById('password').value = password.substr(1, password.length - 2);
 		document.getElementById('Submit1').click();	
 	}
